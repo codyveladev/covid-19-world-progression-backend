30aba303824292bf48adba68e5abbbae3b29c686
Cody Vela <codyvela0702@gmail.com>
2022-02-11 22:35:02 -0600

    added success tests

--- test.py
+++ test.py
@@ -5,22 +5,34 @@ try:
 except Exception as e: 
     print("Some modules are missing {}".format(e))
 
+
+
 class FlaskRestTest(unittest.TestCase):
-    #test the index route 
-    def test_index_route(self): 
+    #util function
+    def route_success_util(self, route): 
         tester = app.test_client(self)
-        response = tester.get('/')
+        response = tester.get(route)
         response_code = response.status_code
         #Ensure the route is working 
-        self.assertEqual(str(response_code), "200")
+        return response_code
     
+    #test the index route 
+    def test_index_route_success(self): 
+        response_code = self.route_success_util('/')
+        self.assertEqual(str(response_code), "200")
+
     #Ensure the response is JSON 
     def test_iso_code_content(self): 
         tester = app.test_client(self)
         response = tester.get('/api/country/USA')
         self.assertEqual(response.content_type, "application/json")
     
-    #Given the iso code in the 
+    #test the iso code route 
+    def test_iso_code_route_success(self):
+        response_code = self.route_success_util('/api/country/USA')
+        self.assertEqual(str(response_code), "200")    
+    
+    #Given the iso code route is returning accurate data 
     def test_iso_code_route(self): 
         tester = app.test_client(self)
         iso_codes = ["USA", "ARG", "RUS"]
@@ -32,10 +44,6 @@ class FlaskRestTest(unittest.TestCase):
             formatted_response = dict(json.loads(response_body.decode('utf8').replace("'", '"')))
             #make sure that the ISO code is giving the correct country 
             self.assertEqual(formatted_response['country'], countries[i])
-
-
-
-
 
 
 if __name__ == "__main__": 
